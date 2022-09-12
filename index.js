6f55b3b2f2b956a83b03eaff501c7a7fffd09cef
Tarun Kumar <80273650+tarun846@users.noreply.github.com>
2022-09-12 11:22:52 +0000

    little change

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 const readlinesync = require("readline-sync");
 const userName = readlinesync.question("What is your name? ");
-console.log("Welcome "+userName+"! Lets see how well do you know me!");
+console.log("Welcome " + userName + "! Lets see how well do you know me!");
 
-let  score = 0;
+let score = 0;
 
 const questionAnswerList = [{
-  question : "How old am I? ",
-  answer : "19"
-},                     {
-    question:"My favourite SuperHero would be? ",
-    answer:"FATHER"
-  },
+  question: "How old am I? ",
+  answer: "20"
+}, {
+  question: "My favourite SuperHero would be? ",
+  answer: "FATHER"
+},
 
 {
-  question : "Where do I live? ",
-  answer : "PURI"
+  question: "Where do I live? ",
+  answer: "PURI"
 },
 {
-  question : "Which is my favorite color? ",
-  answer : "ORANGE"
+  question: "Which is my favorite color? ",
+  answer: "ORANGE"
 },
 {
-  question : "Which is my favorite food? ",
-  answer : "PANNER"
+  question: "Which is my favorite food? ",
+  answer: "PANNER"
 }]
 
-for(let i=0; i<questionAnswerList.length; i++){
+for (let i = 0; i < questionAnswerList.length; i++) {
   var userAnswer = readlinesync.question(questionAnswerList[i].question);
-  if(userAnswer.toUpperCase() === questionAnswerList[i].answer){
-    score+=1;
+  if (userAnswer.toUpperCase() === questionAnswerList[i].answer) {
+    score += 1;
     console.log("Correct!\nCurrent Score : " + score);
     console.log("-------------------------");
   }
-  else{
+  else {
     console.log("Inorrect!\nCurrent Score : " + score);
     console.log("-------------------------");
   }
 }
 
 console.log("Your Total Score : " + score);
-console.log("See the Highest Scorers : ");
 
 
